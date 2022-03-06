2cf07d61ee5e10605642bc5e529ede6851dab190
Brian Aditya <brianadit33@gmail.com>
2022-03-06 10:53:24 +0700

    add new method

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import math
 
 
 class handDetector():
@@ -16,6 +17,8 @@ class handDetector():
     self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
     self.mpDraw = mp.solutions.drawing_utils
 
+    self.tipHands = [4, 8, 12, 16, 20]
+
   def findHands(self, img, draw = True):
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     self.results = self.hands.process(imgRGB)
@@ -30,7 +33,11 @@ class handDetector():
     return img
   
   def findPosition(self, img, handNo=0, draw = True):
-    lmList = []
+    xList = []
+    yList = []
+    bbox = []
+    
+    self.lmList = []
 
     if self.results.multi_hand_landmarks:
       myHand = self.results.multi_hand_landmarks[handNo]
@@ -38,12 +45,56 @@ class handDetector():
       for id, lm in enumerate(myHand.landmark):
         h, w, c = img.shape
         cx, cy = int(lm.x * w), int(lm.y * h)
-        lmList.append([id, cx, cy])
+        self.lmList.append([id, cx, cy])
+        xList.append(cx)
+        yList.append(cy)
         # Circles for one of the landmarks
         if draw:
           cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+      
+      xmin, xmax = min(xList), max(xList)
+      ymin, ymax = min(yList), max(yList)
+
+      bbox = xmin, ymin, xmax, ymax
+
+      if draw:
+        cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20), (255,0,8), 2)
     
-    return lmList
+    return self.lmList, bbox
+
+  def fingersUp(self):
+    fingers = []
+    
+    # For Thumb
+    if self.lmList[self.tipHands[0]][1] < self.lmList[self.tipHands[0]-1][1]:
+      fingers.append(0)
+    else:
+      fingers.append(1)
+
+    # For Fingers
+    for id in range(1, 5):
+      if self.lmList[self.tipHands[id]][2] < self.lmList[self.tipHands[id]-2][2]:
+        fingers.append(1)
+      else:
+        fingers.append(0)
+    
+    return fingers
+  
+  def findDistance(self, p1, p2, img, draw = True, r = 10, t = 2):
+    # Particular landmark
+    x1, y1 = self.lmList[p1][1][1:]
+    x2, y2 = self.lmList[p1][1][1:]
+    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+    if draw:
+      cv2.circle(img, (x1, y1), r, (255, 0, 8), cv2.FILLED)
+      cv2.circle(img, (x2, y2), r, (255, 0, 8), cv2.FILLED)
+      cv2.circle(img, (cx, cy), r, (255, 0, 255), cv2.FILLED)
+      cv2.line(img, (x1, y1), (x2, y2), (255, 0 , 8), t)
+
+    length = math.hypot((x2 - x1), (y2 - y1))
+
+    return length, img, [x1, y1, x2, y2, cx, cy]
 
 
 def main():
@@ -70,7 +121,7 @@ def main():
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
- 
+
     # Frame rate display
     cv2.putText(img, str(int(fps)), (10,40), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 8), 2)
 
